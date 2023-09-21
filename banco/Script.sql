42fd2ea417160cce4efa5cbaa6995182631c6bcd
luisFernandoON <luis123fernandonaves@gmail.com>
2023-09-21 14:57:51 -0300

    Atualização das colunas decimais

--- banco/Script.sql
+++ banco/Script.sql
@@ -5,18 +5,21 @@ USE tabela_periodica;
 CREATE TABLE IF NOT EXISTS elementos(
 numero_atomico INT PRIMARY KEY NOT NULL,
 nome VARCHAR(20) UNIQUE NOT NULL,
-sigla VARCHAR(2) UNIQUE NOT NULL,
-massa DECIMAL NOT NULL,
-ponto_de_fusao DECIMAL NOT NULL,
-ponto_de_ebulicao DECIMAL NOT NULL,
-densidade DECIMAL NOT NULL,
-periodo INT NOT NULL,
-familia INT NOT NULL,
-ano_descoberta VARCHAR(20) NOT NULL,
-escala_de_agressividade DECIMAL NOT NULL,
-escala_de_reutilizacao DECIMAL NOT NULL,
-onde_e_encontrado_na_natureza VARCHAR(200) NOT NULL,
-aplicacoes_do_material VARCHAR(200) NOT NULL
+sigla VARCHAR(3) UNIQUE NOT NULL,
+massa DECIMAL(10,3),
+ponto_de_fusao DECIMAL(10,3),
+ponto_de_ebulicao DECIMAL(10,3),
+densidade DECIMAL(10,3),
+periodo INT,
+familia INT,
+ano_descoberta VARCHAR(20),
+escala_de_agressividade DECIMAL(10,3),
+escala_de_reutilizacao DECIMAL(10,3),
+onde_e_encontrado_na_natureza VARCHAR(200),
+aplicacoes_do_material VARCHAR(200)
 );
 
+# !lembrar de devolver os NOT NULL no futuro!
+
 # DROP DATABASE tabela_periodica;
+# DROP TABLE elementos;
