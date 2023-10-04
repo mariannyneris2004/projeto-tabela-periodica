2f9b535b39bf9b60f270142e14318864aff34db9
mariannyneris2004 <mariannyneris2004@gmail.com>
2023-10-03 22:07:28 -0300

    - Atualização e conexão entre frontend e backend

--- api/src/main/java/periodicfive/api/model/DadosListagemRecord.java
+++
@@ -1,7 +0,0 @@
-package periodicfive.api.model;
-
-public record DadosListagemRecord(int numeroAtomico, String nome, String sigla) {
-    public DadosListagemRecord(Elemento elemento) {
-        this(elemento.getNumeroAtomico(), elemento.getNome(), elemento.getSigla());
-    }
-}
