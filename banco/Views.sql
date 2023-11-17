f6932624f9dd07ef2b27b750085244c9bfa1e84b
DaiannyEvillin <Daiannyevillin@gmail.com>
2023-11-17 20:59:35 -0300

    Views e renomeações

---
+++ banco/Views.sql
@@ -0,0 +1,90 @@
+CREATE VIEW view_familia_1 AS
+SELECT numero_atomico, nome, sigla
+FROM elementos where familia = 1;
+
+CREATE VIEW view_familia_2 AS
+SELECT numero_atomico, nome, sigla
+FROM elementos where familia = 2;
+
+CREATE VIEW view_familia_3 AS
+SELECT numero_atomico, nome, sigla
+FROM elementos where familia = 3;
+
+CREATE VIEW view_familia_4 AS
+SELECT numero_atomico, nome, sigla
+FROM elementos where familia = 4;
+
+CREATE VIEW view_familia_5 AS
+SELECT numero_atomico, nome, sigla
+FROM elementos where familia = 5;
+
+CREATE VIEW view_familia_6 AS
+SELECT numero_atomico, nome, sigla
+FROM elementos where familia = 6;
+
+CREATE VIEW view_familia_7 AS
+SELECT numero_atomico, nome, sigla
+FROM elementos where familia = 7;
+
+CREATE VIEW view_familia_8 AS
+SELECT numero_atomico, nome, sigla
+FROM elementos where familia = 8;
+
+CREATE VIEW view_familia_9 AS
+SELECT numero_atomico, nome, sigla
+FROM elementos where familia = 9;
+
+CREATE VIEW view_familia_10 AS
+SELECT numero_atomico, nome, sigla
+FROM elementos where familia = 10;
+
+CREATE VIEW view_familia_11 AS
+SELECT numero_atomico, nome, sigla
+FROM elementos where familia = 11;
+
+CREATE VIEW view_familia_12 AS
+SELECT numero_atomico, nome, sigla
+FROM elementos where familia = 12;
+
+CREATE VIEW view_familia_13 AS
+SELECT numero_atomico, nome, sigla
+FROM elementos where familia = 13;
+
+CREATE VIEW view_familia_14 AS
+SELECT numero_atomico, nome, sigla
+FROM elementos where familia = 14;
+
+CREATE VIEW view_familia_15 AS
+SELECT numero_atomico, nome, sigla
+FROM elementos where familia = 15;
+
+CREATE VIEW view_familia_16 AS
+SELECT numero_atomico, nome, sigla
+FROM elementos where familia = 16;
+
+CREATE VIEW view_familia_17 AS
+SELECT numero_atomico, nome, sigla
+FROM elementos where familia = 17;
+
+CREATE VIEW view_familia_18 AS
+SELECT numero_atomico, nome, sigla
+FROM elementos where familia = 18;
+
+SELECT * FROM view_familia_1;
+SELECT * FROM view_familia_2;
+SELECT * FROM view_familia_3;
+SELECT * FROM view_familia_4;
+SELECT * FROM view_familia_5;
+SELECT * FROM view_familia_6;
+SELECT * FROM view_familia_7;
+SELECT * FROM view_familia_8;
+SELECT * FROM view_familia_9;
+SELECT * FROM view_familia_10;
+SELECT * FROM view_familia_11;
+SELECT * FROM view_familia_12;
+SELECT * FROM view_familia_13;
+SELECT * FROM view_familia_14;
+SELECT * FROM view_familia_15;
+SELECT * FROM view_familia_16;
+SELECT * FROM view_familia_17;
+SELECT * FROM view_familia_18;
